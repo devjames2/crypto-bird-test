3ad4077b0c1e50625992706d4f9170b90fb62faa
myhency <hency.yeo@gmail.com>
2021-11-21 22:33:43 +0900

    initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,33 @@
+require("@nomiclabs/hardhat-waffle");
+const projectId = "92433e74add243f2833a1634b04a9de7";
+const fs = require("fs");
+const keyData = fs.readFileSync("./p-key.txt", {
+  encoding: "utf8",
+  flag: "r",
+});
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337, // config standard
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
+      accounts: [keyData],
+    },
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${projectId}`,
+      accounts: [keyData],
+    },
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+};
